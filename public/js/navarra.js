5ee51ed3ffef2deac1011aa37e1791e65a4b797e
Alexander <aaltzibar007@gmail.com>
2014-12-16 08:17:28 +0100

    cambios visuales

--- public/js/navarra.js
+++ public/js/navarra.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 			url: "/navarra",
 			dataType: "json",
 			success: function(data){
-				 $("#vinedo1").css('background-image','url('+data[0].imagen+')');
+				 $("#vinedo1").css('background-image','url('+'http://www.mediafire.com/convkey/fd28/edpn2c1f0p3gnzz6g.jpg'+')');
 				 $("#vinedo2").css('background-image','url('+data[1].imagen+')');					 
 				 $("#vinedo3").css('background-image','url('+data[2].imagen+')');			 	 
 			}
