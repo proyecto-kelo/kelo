c53693638bbc7970e6cb3869fab5f060db1e8bdf
root <root@zubiri-VirtualBox.(none)>
2015-01-22 11:13:21 +0100

    form

---
+++ public/js/formulario.js
@@ -0,0 +1,31 @@
+// Funcion de formulario que recoge los datos
+function enviar(){
+    var nombre = document.getElementById('nombre').value;
+    var arraySplit = nombre.split("");
+    if (nombre.length==0){
+        alert("nombre esta vacio");
+    }else{
+        var asunto = document.getElementById('asunto').value;
+        var arraySplit = asunto.split("");
+        if (asunto.length==0){
+            alert("Asunto esta vacio");
+        }else{
+            var mensaje = document.getElementById('mensaje').value;
+            var arraySplit = mensaje.split("");
+            if (mensaje.length==0){
+                alert("Mensaje esta vacio");
+            }else{
+                $.ajax({
+                    type: "POST",
+                    url: "enviar.php",
+                    data: {$("#formulario").serialize()},
+                    success: function(data){
+                        alert(data);
+                    }
+                });
+            }
+        } 
+    }
+    
+    
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,4 @@
-// Funcion de formulario que recoge los datos
-function enviar(){
-    var nombre = document.getElementById('nombre').value;
-    var arraySplit = nombre.split("");
-    if (nombre.length==0){
-        alert("nombre esta vacio");
-    }
-    var asunto = document.getElementById('asunto').value;
-    var arraySplit = asunto.split("");
-    if (asunto.length==0){
-        alert("Asunto esta vacio");
-    } 
-    var mensaje = document.getElementById('mensaje').value;
-    var arraySplit = mensaje.split("");
-    if (mensaje.length==0){
-        alert("Mensaje esta vacio");
-    }
-}
+
 // Funcion para mostrar el pop-up del viñedo seleccionado
 function mostrar(nombre){
     $('.bodegas').css({opacity:0});
